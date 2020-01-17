a3a33716f2405490d22db7e741ba9b56b9f44f40
liaoyinglong <vigossliao@gmail.com>
2020-01-17 17:55:50 +0800

    :sparkles: add 0069

---
+++ src/0069.x 的平方根/index.js
@@ -0,0 +1,31 @@
+"use strict";
+/**
+ 实现 int sqrt(int x) 函数。
+
+ 计算并返回 x 的平方根，其中 x 是非负整数。
+
+ 由于返回类型是整数，结果只保留整数的部分，小数部分将被舍去。
+
+ 示例 1:
+
+ 输入: 4
+ 输出: 2
+ 示例 2:
+
+ 输入: 8
+ 输出: 2
+ 说明: 8 的平方根是 2.82842...,
+      由于返回类型是整数，小数部分将被舍去。
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/sqrtx
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+/**
+ * @param {number} x
+ * @return {number}
+ */
+const mySqrt = function (x) {
+    return Math.floor(Math.sqrt(x));
+};
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJpbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBQUE7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7OztHQXFCRztBQUVIOzs7R0FHRztBQUNILE1BQU0sTUFBTSxHQUFHLFVBQVMsQ0FBUztJQUMvQixPQUFPLElBQUksQ0FBQyxLQUFLLENBQUMsSUFBSSxDQUFDLElBQUksQ0FBQyxDQUFDLENBQUMsQ0FBQyxDQUFDO0FBQ2xDLENBQUMsQ0FBQyJ9
