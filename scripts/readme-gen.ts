4ac0a05c298c63c2acead285fcea65facf147940
liaoyl <liaoyl@akulaku.com>
2021-07-22 16:32:39 +0800

    :construction: Work in progress.

---
+++ scripts/readme-gen.ts
@@ -0,0 +1,26 @@
+import * as fs from "fs-extra";
+import * as path from "path";
+
+async function gen(type: "completed" | "todo") {
+  const dirs = await fs.readdir(path.join(__dirname, `../src/${type}`));
+
+  const str = dirs
+    .map((item) => {
+      return `- [x] ${item}`;
+    })
+    .join("\n");
+  console.log(`${type}: 共 ${dirs.length}题`);
+
+  return `# ${type}` + "\n\n" + str;
+}
+
+async function main() {
+  const completed = await gen("completed");
+  const todo = await gen("todo");
+
+  const readmePath = path.join(__dirname, "../readme.md");
+
+  await fs.writeFile(readmePath, todo + "\n\n" + completed);
+}
+
+main();
